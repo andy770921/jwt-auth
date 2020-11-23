45c6bc395b783e3eab81fc9731d8b0a60b2b82b0
andy <andy770921@yahoo.com.tw>
2020-11-23 18:12:54 +0800

    feat: add eslintrc.json rule

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,7 @@ app.get('/', (req, res) => res.send('Home Page Route'));
 
 app.get('/about', (req, res) => res.send('About Page Route'));
 
-app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
-
-app.get('/contact', (req, res) => res.send('Contact Page Route'));
+app.get('/api', (req, res) => res.status(200).json({ data: 'api' }));
 
 const port = process.env.PORT || 3000;
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,30 @@
-const path = require('path'); 
+const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 const NodemonPlugin = require('nodemon-webpack-plugin');
 
 module.exports = {
-    entry: './src/app.ts',
-    target: "node",
-    externals: [nodeExternals()],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'app.js'
-    },
-    plugins: [
-        new NodemonPlugin({
-            watch: path.resolve('./dist')
-        })
+  entry: './src/app.ts',
+  target: 'node',
+  externals: [nodeExternals()],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js',
+  },
+  plugins: [
+    new NodemonPlugin({
+      watch: path.resolve('./dist'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.ts','.js']
-    }
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
 };
