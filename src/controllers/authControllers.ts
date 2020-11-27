a2e2de057b8b077b4aaba3c5782d42590ded1d3c
andy <andy770921@yahoo.com.tw>
2020-11-27 18:25:09 +0800

    feat: finish error handling

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -1,5 +1,37 @@
 import { Request, Response } from 'express';
+import { Error as MongooseError } from 'mongoose';
 import UserModel from '../models/user';
+
+interface ValidationError extends Error {
+  message: string;
+  code: number;
+  errors: {
+    email: MongooseError.ValidatorError,
+    password: MongooseError.ValidatorError
+  },
+  _message: string;
+}
+
+interface ErrorNameMessageMapping {
+  email: string;
+  password: string;
+}
+
+const handleErrors = (err: ValidationError): ErrorNameMessageMapping => {
+  // duplicate error code
+  if (err.code === 11000) {
+    return { email: 'that email is already registered', password: '' };
+  }
+  // validation errors
+  if (err.message.includes('user validation failed')) {
+    const errorCollection = Object.values(err.errors).reduce((collection, errorItem) => (
+      { ...collection, [errorItem.path]: errorItem.properties.message }
+    ), {});
+    return errorCollection as ErrorNameMessageMapping;
+  }
+
+  return { email: '', password: '' };
+};
 
 export const signupGet = (req: Request, res: Response) => res.status(200).json({ data: 'signup get' });
 
@@ -9,8 +41,8 @@ export const signupPost = async (req: Request, res: Response) => {
     const { _id: createdId, email: createdEmail } = await UserModel.create({ email, password });
     res.status(201).json({ result: 'Success', user: { id: createdId, email: createdEmail } });
   } catch (err) {
-    console.log('Signup Post error: ', err);
-    res.status(400).json({ reason: 'createUserFail' });
+    const errorDetail = handleErrors(err);
+    res.status(400).json({ reason: 'createUserFail', data: errorDetail });
   }
 };
 
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,5 @@
 import { Schema, model, Document } from 'mongoose';
+import { isEmail } from 'validator';
 
 interface User extends Document {
   email: string;
@@ -8,14 +9,15 @@ interface User extends Document {
 const userSchema = new Schema({
   email: {
     type: String,
-    required: true,
+    required: [true, 'Please enter an email'],
     unique: true,
     lowercase: true,
+    validate: [isEmail, 'Please enter a valid email'],
   },
   password: {
     type: String,
-    required: true,
-    minlength: 6,
+    required: [true, 'Please enter an password'],
+    minlength: [6, 'Minimum password length is 6 characters'],
   },
 });
 
